9f9a7be0b7c8e2284befd1bc34894753cce8501b
Bug <36928088+AntoTheBug@users.noreply.github.com>
2021-02-10 08:26:24 +0100

    Update admin.py

--- final_template/onlinecourse/admin.py
+++ final_template/onlinecourse/admin.py
@@ -1,25 +1,27 @@
 from django.contrib import admin
 # <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner, Question, Choice
+from .models import Course, Lesson, Question, Choice, Instructor, Learner, Submission
 
 # <HINT> Register QuestionInline and ChoiceInline classes here
 
 
-class LessonInline(admin.StackedInline):
-    model = Lesson
-    extra = 5
-
 class QuestionInline(admin.StackedInline):
     model = Question
     extra = 5
+
     
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 5
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    inlines = [LessonInline, QuestionInline, ChoiceInline]
+    inlines = [LessonInline]
     list_display = ('name', 'pub_date')
     list_filter = ['pub_date']
     search_fields = ['name', 'description']
@@ -29,11 +31,16 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text', 'grade']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text', 'is_correct']
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
-admin.site.register(Question)
-admin.site.register(Choice)
